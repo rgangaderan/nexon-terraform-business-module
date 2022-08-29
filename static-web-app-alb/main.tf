28db5708c3a8f470416e83654032ea5c052eed7b
rajee-gangadaran <rajee.gangadaran@bitwala.com>
2022-08-30 01:18:48 +0530

    feat: ALB with EC2 for StaticWebApp

--- static-web-app-alb/main.tf
+++ static-web-app-alb/main.tf
@@ -3,7 +3,6 @@
 # Application LoadBalancer
 ################################################################################
 module "application_load_balancer" {
-
   source = "git@github.com:rgangaderan/nexon-terraform-tech-module.git//aws/alb?ref=v2.2.0"
 
   network     = var.network
@@ -14,31 +13,23 @@ module "application_load_balancer" {
 }
 
 ################################################################################
-# EC2 Autoscaling Group
+# EC2 Private
 ################################################################################
 
-module "ec2_autoscaling" {
-
-  source = "git@github.com:rgangaderan/nexon-terraform-tech-module.git//aws/ec2_autoscaling?ref=v2.2.0"
-
+module "ec2" {
+  source = "/Users/rajee/Desktop/new-nexon/nexon-terraform-tech-module/aws/ec2_private"
 
   image_id             = data.aws_ami.ubuntu.id
   instance_type        = var.instance_type
+  instance_count       = var.instance_count
   key_name             = var.key_name
   user_data            = file("user_data.sh")
-  volume_size          = var.volume_size
-  volume_type          = var.volume_type
   subnet_id            = var.subnet_id
   security_groups      = [aws_security_group.instances.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   stage                = var.stage
   name                 = var.name
-  vpc_zone_identifier  = var.vpc_zone_identifier
-  max_size             = var.max_size
-  min_size             = var.min_size
-  target_group_arns    = [module.application_load_balancer.target_group_arns]
 
-  depends_on = [module.application_load_balancer]
 
   providers = {
     aws = aws
---
+++ static-web-app-alb/outputs.tf
@@ -0,0 +1,5 @@
+output "instance_ids" {
+  value       = module.ec2.instance_ids
+  description = "EC2 Instance IDs"
+
+}
