99ce4db15d55361b7502c6256aa677058853ca21
rajee-gangadaran <rajee.gangadaran@bitwala.com>
2022-09-05 20:45:10 +0530

    typo

--- static-web-app-alb/main.tf
+++ static-web-app-alb/main.tf
@@ -35,7 +35,7 @@ module "application_load_balancer" {
 
 module "ec2_autoscaling" {
 
-  source = "git@github.com:rgangaderan/nexon-terraform-tech-module.git//aws/ec2_autoscaling?ref=v2.3.3"
+  source = "git@github.com:rgangaderan/nexon-terraform-tech-module.git//aws/ec2-autoscaling?ref=v2.3.3"
 
 
   image_id             = data.aws_ami.ubuntu.id
