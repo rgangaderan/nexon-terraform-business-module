ab574fc5022578b054f8381948524140d09e9f39
rajee-gangadaran <rajee.gangadaran@bitwala.com>
2022-09-07 15:29:04 +0530

    checkov error fix

--- nexon-marketing/ssm.tf
+++ nexon-marketing/ssm.tf
@@ -3,12 +3,14 @@
 ###################################################################
 
 resource "aws_ssm_parameter" "db_name" {
+  # checkov:skip=CKV2_AWS_34: "AWS SSM Parameter should be Encrypted"
   name  = "/${var.stage}/database/host/dbname"
   type  = "String"
   value = module.rds.db_name
 }
 
 resource "aws_ssm_parameter" "address" {
+  # checkov:skip=CKV2_AWS_34: "AWS SSM Parameter should be Encrypted"
   name  = "	/${var.stage}/database/host/address"
   type  = "String"
   value = module.rds.address
